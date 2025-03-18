b5ab6523030d60c0041570615e89805e125bb766
Asfand Yar <asfandium.1@gmail.com>
2025-03-18 03:09:40 -0700

    removed code for create and edit

--- MansehraPaintHouse/MansehraPaintHouse.Admin/Controllers/CategoryController.cs
+++ MansehraPaintHouse/MansehraPaintHouse.Admin/Controllers/CategoryController.cs
@@ -22,104 +22,6 @@ namespace MansehraPaintHouse.Admin.Controllers
             return View("CategoryIndex", categories);
         }
 
-        //public IActionResult Create()
-        //{
-        //    ViewBag.ParentCategories = _context.Categories.ToList(); // Pass parent categories for dropdown
-        //    return View("CategoryCreate");
-        //}
-
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public IActionResult Create(Category category, IFormFile? Image1File, IFormFile? Image2File)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        // Save images to the specified folder and store their paths
-        //        if (Image1File != null)
-        //        {
-        //            var imagePath = Path.Combine("wwwroot/images", Image1File.FileName);
-        //            using (var stream = new FileStream(imagePath, FileMode.Create))
-        //            {
-        //                Image1File.CopyTo(stream);
-        //            }
-        //            category.Image1 = $"/images/{Image1File.FileName}";
-        //        }
-
-        //        if (Image2File != null)
-        //        {
-        //            var imagePath = Path.Combine("wwwroot/images", Image2File.FileName);
-        //            using (var stream = new FileStream(imagePath, FileMode.Create))
-        //            {
-        //                Image2File.CopyTo(stream);
-        //            }
-        //            category.Image2 = $"/images/{Image2File.FileName}";
-        //        }
-
-        //        // Save the category to the database
-        //        _context.Categories.Add(category);
-        //        _context.SaveChanges();
-
-        //        return RedirectToAction("Index");
-        //    }
-
-        //    ViewBag.ParentCategories = _context.Categories.ToList(); // Pass parent categories for dropdown
-        //    return View("CategoryCreate", category); 
-        //}
-
-        //public IActionResult Edit(int id)
-        //{
-        //    var category = _context.Categories.Find(id);
-        //    if (category == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    ViewBag.ParentCategories = _context.Categories.ToList();
-        //    return View("CategoryEdit", category); 
-        //}
-
-        //[HttpPost]
-        //public IActionResult Edit(Category category, IFormFile? Image1File, IFormFile? Image2File)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        var existingCategory = _context.Categories.Find(category.CategoryID);
-        //        if (existingCategory != null)
-        //        {
-        //            existingCategory.Name = category.Name;
-        //            existingCategory.Description = category.Description;
-        //            existingCategory.ParentCategoryID = category.ParentCategoryID;
-        //            existingCategory.IsActive = category.IsActive;
-
-        //            // Update images if new ones are uploaded
-        //            if (Image1File != null)
-        //            {
-        //                existingCategory.Image1 = SaveImage(Image1File);
-        //            }
-
-        //            if (Image2File != null)
-        //            {
-        //                existingCategory.Image2 = SaveImage(Image2File);
-        //            }
-
-        //            _context.SaveChanges(); // Save changes to the database
-        //        }
-        //        return RedirectToAction("Index");
-        //    }
-
-        //    ViewBag.ParentCategories = _context.Categories.ToList();
-        //    return View("CategoryEdit", category); 
-        //}
-
-
-
-
-
-
-
-
-
-
         public IActionResult Upsert(int? id)
         {
             var category = id == null || id == 0
@@ -179,24 +81,6 @@ namespace MansehraPaintHouse.Admin.Controllers
             ViewBag.ParentCategories = _context.Categories.ToList(); // Pass parent categories for dropdown
             return View("CategoryUpsert", category);
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
         public IActionResult Delete(int id)
         {
